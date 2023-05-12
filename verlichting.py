b0a0fd7d803dceb0aef9fe67b4c52cfab291b8b4
Damyan Semedo Robalo <r0915080@student.thomasmore.be>
2023-05-12 13:16:44 +0200

    verlichting

---
+++ verlichting.py
@@ -0,0 +1,42 @@
+import time
+import board
+import neopixel
+import sqlite3
+
+pixels1 = neopixel.NeoPixel(board.D18, 55, brightness=1)
+
+book_id = int(input("Id of the book: "))
+conn = sqlite3.connect('db_file')
+
+to_execute_sql_command = conn.cursor()
+
+
+# timing
+def timing_from_databank():
+    sql_query_time = """SELECT lightTiming 
+                        FROM db_file.LightBook 
+                        WHERE bookId = ?"""
+    to_execute_sql_command.execute(sql_query_time, (book_id))
+
+    timing = to_execute_sql_command.fetchone()
+    return timing
+
+
+#color
+def color_from_databank():
+    sql_query_color = """SELECT lightFilePath 
+            FROM db_file.Light 
+            WHERE lightId in
+                (SELECT lightId
+                 FROM LightBook 
+                 WHERE bookId = ?)"""
+    to_execute_sql_command.execute(sql_query_color, (book_id))
+
+    color_tuple = tuple(map(int, sql_query_color.split(",")))
+    return color_tuple
+
+
+time.sleep(timing_from_databank)
+pixels1.fill(color_from_databank)
+
+
